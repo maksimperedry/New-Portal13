82bdb55eca1c12f971ed5845c10c737ca5b3807b
maksimperedry <144063148+maksimperedry@users.noreply.github.com>
2023-12-13 19:15:52 +0300

    Add files via upload

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -1,29 +1,10 @@
-"""NewsPortal URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import Products, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, NewsPortal
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # чтобы адреса в будущем написанных нами страничек были доступны
-    # нам для перехода по ним, добавим путь:
-    path('pages/', include('django.contrib.flatpages.urls')),
-
-    # чтобы все адреса из файла NewsPaper/urls.py
-    # сами автоматически подключались когда мы их добавим.
-    path('news/', include('NewsPaper.urls')),
+    path('', Products.as_view()),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Ссылка на детали товара
+    path('create/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товара
 ]
+path('create/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
